1438a0cc2437f413028c0d462bdea05e0ac79db7
Puyih Severino <yihseverino@gmail.com>
2019-05-02 07:35:58 +0800

    First commit

---
+++ learning-playground/projects/vue_with_brad/src/components/Todos.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-on:del-todo="$emit('del-todo', todo.id)" v-bind:todo ="todo"/>
+        </div>    
+    </div>
+</template>
+<script>
+
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: 'Todos',
+    components: {
+        TodoItem
+    },
+    methods: {
+        test: function(){ console.log('called 123')}
+    },
+    props: ['todos']
+}
+</script>
+
+<style scoped>
+
+</style>
+
+// TODO
+// learn modules for es6 again
